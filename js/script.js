0075884e2ff8140935773da3f4fe2f2a54f7942a
TheAnalystAM <theanalystam@gmail.com>
2024-08-13 16:34:52 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,6 +8,97 @@ $(document).ready(function(){ // begin document.ready block
 
 
 
+// Update Notes: Edit This to Display the Most Eye Catching Story When Clicked in News Teaser, The Latest, Regional Markets News
+
+
+    // NEWS TEASER
+
+    $('.topnewsteaser').click(function(){
+        window.location.href = "/tabs/about/;
+    });
+
+
+
+
+// The Latest
+
+
+    $('#thelateststories1').click(function(){
+
+        window.location.href = "/tabs/about/;
+    });
+
+
+    $('#thelateststories2').click(function(){
+
+        window.location.href = "/tabs/about/;
+    });
+
+
+    $('#thelateststories3').click(function(){
+
+        window.location.href = "/tabs/about/;
+    });
+
+
+    $('#thelateststories4').click(function(){
+
+        window.location.href = "/tabs/about/;
+    });
+
+
+
+// Top Picks
+
+
+
+    $('#toppicksstories1').click(function(){
+
+        window.location.href = "/stories/markets/s2bumbledrop/;
+    });
+
+
+
+// Regional Markets News
+
+
+    // US/North America News
+
+
+    $('#usnewsstories1').click(function(){
+
+        window.location.href = "/stories/markets/s2bumbledrop/;
+    });
+
+    $('#usnewsstories2').click(function(){
+
+        window.location.href = "/stories/markets/s2bumbledrop/;
+    });
+
+    $('#usnewsstories3').click(function(){
+
+       window.location.href = "/stories/markets/s2bumbledrop/;
+    });
+
+
+    // Global Markets
+
+    $('#globalnewsstories1').click(function(){
+
+        window.location.href = "/stories/markets/s2bumbledrop/;
+    });
+
+    $('#globalnewsstories2').click(function(){
+
+        window.location.href = "/stories/markets/s2bumbledrop/;
+    });
+
+    $('#globalnewsstories3').click(function(){
+
+        window.location.href = "/stories/markets/s2bumbledrop/;
+    });
+
+
 
 // General Content
 
@@ -69,6 +160,46 @@ $(document).ready(function(){ // begin document.ready block
         $('.overlay').removeClass('show');
         $('#closeMenu').hide();
         $('#openMenu').show();
+    });
+
+
+
+
+   HOMEPAGE CONTENT - Not Connector Code
+
+    $('.subscribe').click(function(){
+         
+         var email = $('.emailinput').val();
+            if(email) {
+               
+               $.ajax({
+                  url: 'https://script.google.com/macros/s/AKfycbzqEj2dpy_qsRGywNb6w3Q96rBALK7GMvJNkFY5_eVOGfNmUt8XPMaMIv1j7EmA2xx_/exec', // Replace with your Google Apps Script web app URL
+                  type: 'POST',
+                  data: {
+                     email: email
+                  },
+               success: function(response) {
+                  console.log('Email added:', response);
+                  $('.emailinput').val('');
+
+                  $('.thankyou').toggle();
+
+               },
+               error: function(error) {
+                  console.error('Error:', error);
+               }
+            });
+
+      } else {
+         console.log('Please enter an email address.');
+      }
+
+    });
+
+    $('.subscribe').click(function(){
+
+    $('.newslettersubscribe').css('height', '700px');
+
     });
 
 
