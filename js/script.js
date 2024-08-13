c764975de47401ce7666777116ecde9b6e78c7e4
TheAnalystAM <theanalystam@gmail.com>
2024-08-13 16:11:27 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,118 @@
 
 $(document).ready(function(){ // begin document.ready block
 
+// Update Notes: Edit This to Display the Most Eye Catching Story When Clicked in News Teaser, The Latest, Regional Markets News
 
+
+    // NEWS TEASER
+
+    $('.topnewsteaser').click(function(){
+       
+        window.location.href = "/tabs/about/";
+       
+    });
+
+
+
+
+// The Latest
+
+
+    $('#thelateststories1').click(function(){
+
+        window.location.href = "/tabs/about/";
+       
+    });
+
+
+    $('#thelateststories2').click(function(){
+
+        window.location.href = "/tabs/about/";
+
+        $('html, body').scrollTop(0);
+    });
+
+
+    $('#thelateststories3').click(function(){
+
+        window.location.href = "/tabs/about/";
+
+        $('html, body').scrollTop(0);
+    });
+
+
+    $('#thelateststories4').click(function(){
+
+        window.location.href = "/tabs/about/";
+
+        $('html, body').scrollTop(0);
+    });
+
+
+
+// Top Picks
+
+
+
+    $('#toppicksstories1').click(function(){
+
+        window.location.href = "/tabs/about/";
+
+        $('html, body').scrollTop(0);
+    });
+
+
+
+// Regional Markets News
+
+
+    // US/North America News
+
+
+    $('#usnewsstories1').click(function(){
+
+        window.location.href = "/tabs/about/";
+
+        $('html, body').scrollTop(0);
+    });
+
+    $('#usnewsstories2').click(function(){
+
+        window.location.href = "/tabs/about/";
+
+        $('html, body').scrollTop(0);
+    });
+
+    $('#usnewsstories3').click(function(){
+
+        window.location.href = "/tabs/about/";
+
+        $('html, body').scrollTop(0);
+    });
+
+
+    // Global Markets
+
+    $('#globalnewsstories1').click(function(){
+
+        window.location.href = "/tabs/about/";
+
+        $('html, body').scrollTop(0);
+    });
+
+    $('#globalnewsstories2').click(function(){
+
+        window.location.href = "/tabs/about/";
+
+        $('html, body').scrollTop(0);
+    });
+
+    $('#globalnewsstories3').click(function(){
+
+        window.location.href = "/tabs/about/";
+
+        $('html, body').scrollTop(0);
+    });
 
 
 
@@ -82,7 +193,46 @@ $(document).ready(function(){ // begin document.ready block
 
 
 
-    // Overlay
+   // HOME PAGE CODE - Non-connector
+
+
+
+    $('.subscribe').click(function(){
+         
+         var email = $('.emailinput').val();
+            if(email) {
+               
+               $.ajax({
+                  url: 'https://script.google.com/macros/s/AKfycbzqEj2dpy_qsRGywNb6w3Q96rBALK7GMvJNkFY5_eVOGfNmUt8XPMaMIv1j7EmA2xx_/exec', // Replace with your Google Apps Script web app URL
+                  type: 'POST',
+                  data: {
+                     email: email
+                  },
+               success: function(response) {
+                  console.log('Email added:', response);
+                  $('.emailinput').val('');
+
+                  $('.thankyou').toggle();
+
+               },
+               error: function(error) {
+                  console.error('Error:', error);
+               }
+            });
+
+      } else {
+         console.log('Please enter an email address.');
+      }
+
+    });
+
+    $('.subscribe').click(function(){
+
+    $('.newslettersubscribe').css('height', '700px');
+
+    });
+
+   
 
 
 
